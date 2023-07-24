ee3fac6e007516384c9ee3bbe9770821ebd4ab3d
laurentfidahoussen <news@pifpafpouf.fr>
2023-07-24 22:27:10 +0200

    last updates

--- src/main/java/com/i28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/i28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -3,7 +3,6 @@ package com.i28minutes.springboot.myfirstwebapp.hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class SayHelloController {
